5e73e97fa78772333894231ea3ecd7fabfd03b0a
tkorays <tkorays@hotmail.com>
2016-04-28 22:07:15 +0800

    update lib

--- plugin/Plugin.c
+++ plugin/Plugin.c
@@ -1,9 +1,49 @@
 #include "plugin.h"
-#include <stdio.h>
+#include <string.h>
 
-Command g_CmdList[100];
-int g_CmdSize = 0;
 
-void CmdExec(const char* name,const char* para_str){
-      printf("load %s and run!\n",name);
+PLG_DyLibFuncSocket* PLG_GetDyLibFuncSocket(){
+    static PLG_DyLibFuncSocket sock;
+    return &sock;
+}
+
+PLG_HANDLE PLG_LoadLib(const char* dllname){
+    PLG_DyLibFuncSocket* sock = PLG_GetDyLibFuncSocket();
+    if(sock->LoadLib){
+        return sock->LoadLib(dllname);
+    }
+    return PLG_NULL;
+}
+
+void PLG_CloseLib(PLG_HANDLE hd){
+    PLG_DyLibFuncSocket* sock = PLG_GetDyLibFuncSocket();
+    if(sock->CloseLib){
+        sock->CloseLib(hd);
+    }
+}
+
+PLG_HANDLE PLG_LibAddr(PLG_HANDLE hd,const char* name){
+    PLG_DyLibFuncSocket* sock = PLG_GetDyLibFuncSocket();
+    if(sock->LibAddr){
+        return sock->LibAddr(hd,name);
+    }
+    return PLG_NULL;
+}
+
+PLG_FUNC PLG_GetFunc(PLG_HANDLE plghd, const char* name){
+    PLG_DyLibFuncSocket* sock = PLG_GetDyLibFuncSocket();
+    PLG_FUNC_INFO* func = (PLG_FUNC_INFO*)sock->LibAddr(plghd,PLG_FUNC_INFO_NAME);
+    int* size = (int*)sock->LibAddr(plghd,PLG_FUNC_SIZE_NAME);
+    int i = 0;
+
+    if(!func || !size || *size<=0){
+        return PLG_NULL;
+    }
+
+    for(i = 0; i<*size; i++){
+        if(strcmp(func[i].name,name)==0){
+            return func[i].func;
+        }
+    }
+    return PLG_NULL;
 }
--- plugin/Plugin.h
+++ plugin/Plugin.h
@@ -1,36 +1,15 @@
-#ifndef __JX_PLUGIN__
-#define __JX_PLUGIN__
+#ifndef __PLUGIN_H__
+#define __PLUGIN_H__
 
-#define MAX_CMD_NAME_SIZE 20
-#define MAX_CMD_NAME_LEN 19
-#define MAX_PARA_NUM 9+1
+#include "plugin_if.h"
+#include "plugin_adp.h"
 
+PLG_DyLibFuncSocket* PLG_GetDyLibFuncSocket();
+PLG_DyLibFuncSocket* PLG_AdpInstallDyLib();
 
+PLG_HANDLE PLG_LoadLib(const char* name);
+void PLG_CloseLib(PLG_HANDLE hd);
+PLG_HANDLE PLG_LibAddr(PLG_HANDLE hd,const char* name);
+PLG_FUNC PLG_GetFunc(PLG_HANDLE hd, const char* name);
 
-
-typedef enum {
-    CPT_NULL,
-    CPT_DIGIT,
-    CPT_STRING
-} CMD_PARA_TYPE;
-
-typedef struct {
-    char name[MAX_CMD_NAME_SIZE];
-    CMD_PARA_TYPE para_type[MAX_PARA_NUM];
-    void* handler;
-} Command;
-
-void CmdExec(const char* name,const char* para_str);
-
-extern Command g_CmdList[100];
-extern int g_CmdSize;
-
-// generate function prototype
-
-#define FUNC_PROTOTYPE(ret,name,args...) \
-  ret(*name)(args)
-
-
-
-
-#endif // __JX_PLUGIN__
+#endif // __PLUGIN_H__
